d4afc9be0ea5cde2d5546d99372a9933c4416498
st084331 <71183199+st084331@users.noreply.github.com>
2021-09-11 17:20:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def pref_func(str):
+    p = [0] * len(str)
+    for i in range(1, len(str)):
+        j = p[i - 1]
+        while j > 0 and str[i] != str[j]:
+            j = p[j - 1]
+        if str[i] == str[j]:
+            j += 1
+        p[i] = j
+    return max(p)
+
+str = "abacabadabacaba"
+length = len(str)
+sum = 0
+for j in range(length):
+    sum += len(str) - pref_func(str)
+    str = str[1:]
+print(sum)
