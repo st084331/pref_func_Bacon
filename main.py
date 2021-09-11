903e20fbce33ba80de077985d76aa70c3cba0ebf
st084331 <71183199+st084331@users.noreply.github.com>
2021-09-11 18:13:22 +0300

    Some simplifications

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def pref_func(str):
     p = [0] * len(str)
+    max = 0
     for i in range(1, len(str)):
         j = p[i - 1]
         while j > 0 and str[i] != str[j]:
@@ -7,12 +8,13 @@ def pref_func(str):
         if str[i] == str[j]:
             j += 1
         p[i] = j
-    return max(p)
+        if (j>max): max = j
+    return max
 
 str = input()
 length = len(str)
 sum = 0
-for j in range(length):
+for j in range(length-1):
     sum += len(str) - pref_func(str)
     str = str[1:]
-print(sum)
+print(sum+1)
